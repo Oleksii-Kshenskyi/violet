0a5d051c08c2132447a3a3d4d7c5d508adc68330
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-12-03 17:31:41 +0200

    (#8) Removed an unnecessary usage of String.

--- src/data/pathtree.rs
+++ src/data/pathtree.rs
@@ -11,18 +11,16 @@ impl PathTree {
       }
    }
 
-   pub fn create_path(pathify_this: &String) -> Vec<String> {
-      pathify_this.trim().split_whitespace().map(|elem| {
-         elem.to_string()
-      }).collect::<Vec<String>>()
+   pub fn create_path(pathify_this: &String) -> Vec<&str> {
+      pathify_this.trim().split_whitespace().collect::<Vec<&str>>()
    }
 
-   pub fn set_by_path(&mut self, element: String, path: Vec<String>) {
+   pub fn set_by_path(&mut self, value: String, path: Vec<&str>) {
       path.iter().enumerate().for_each(|(index, elem)| {
-         match self.tree.get_mut(elem) {
+         match self.tree.get_mut(elem.to_owned()) {
             Some(inner_elem) => {
                if index == path.len() - 1 {
-                  inner_elem.as_mut().unwrap().insert(elem.to_string(), element.clone());
+                  inner_elem.as_mut().unwrap().insert(elem.to_string(), value.clone());
                }
             } ,
             None => { self.tree.insert(elem.to_string(), Some(HashMap::new())); }
